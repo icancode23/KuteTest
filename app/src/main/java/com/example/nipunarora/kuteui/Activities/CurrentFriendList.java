d633048cae51a73cb4a1b289120da8e2f1f7ffdd
icancode23 <ernipunarora@gmail.com>
2017-06-19 02:09:05 +0530

    added the current friend activity

--- app/src/main/java/com/example/nipunarora/kuteui/Activities/CurrentFriendList.java
+++ app/src/main/java/com/example/nipunarora/kuteui/Activities/CurrentFriendList.java
@@ -1,16 +1,14 @@
-package com.example.nipunarora.kuteui.Fragments;
+package com.example.nipunarora.kuteui.Activities;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 
 import com.example.nipunarora.kuteui.Adapters.FriendRecyclerAdapter;
 import com.example.nipunarora.kuteui.DataModels.Person;
@@ -19,50 +17,39 @@ import com.example.nipunarora.kuteui.R;
 import java.util.ArrayList;
 
 /**
- * Created by nipunarora on 06/06/17.
+ * Created by nipunarora on 19/06/17.
  */
 
-public class CurrentFriend extends Fragment {
-    static String TAG="FriendFragment";
-    View v;
+public class CurrentFriendList extends AppCompatActivity {
+    static String TAG="CurrentFriend Activity";
     ArrayList<Person> person_list;
-    
-
-    public CurrentFriend() {
-    }
-
+    RecyclerView friend_recycler;
+    FriendRecyclerAdapter recycler_adapter;
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.current_friends_activity);
+
+        /*************** Creating Sample Friends for now ***********/
         person_list=new ArrayList<Person>();
         for(int i=0;i<4;++i)
         {
             Person temp=new Person("Nipun Arora");
             person_list.add(temp);
         }
-        
-    }
-
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        v=inflater.inflate(R.layout.current_friends_fragment,container,false);
-        return v;
-
-    }
-
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
+        /************** Initialise the views *********/
+        //Setting up a sample bitmap for now
+        //TODO get friends from facebook and google and get their images
         Bitmap icon = BitmapFactory.decodeResource(this.getResources(),
                 R.drawable.samplperson);
         Bitmap scaled = Bitmap.createScaledBitmap(icon, 60, 60, true);
-        RecyclerView friend_recycler=(RecyclerView)v.findViewById(R.id.personRecycler);
-        FriendRecyclerAdapter recycler_adapter=new FriendRecyclerAdapter(person_list,scaled);
-        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getActivity().getApplicationContext());
+        friend_recycler=(RecyclerView)findViewById(R.id.personRecycler);
+        recycler_adapter=new FriendRecyclerAdapter(person_list,scaled);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
         friend_recycler.setLayoutManager(mLayoutManager);
         friend_recycler.setItemAnimator(new DefaultItemAnimator());
         friend_recycler.setAdapter(recycler_adapter);
+
 
     }
 }
--- app/src/main/java/com/example/nipunarora/kuteui/Fragments/FriendTab.java
+++ app/src/main/java/com/example/nipunarora/kuteui/Fragments/FriendTab.java
@@ -1,19 +1,22 @@
 package com.example.nipunarora.kuteui.Fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.AppCompatButton;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.nipunarora.kuteui.Activities.CurrentFriendList;
 import com.example.nipunarora.kuteui.R;
 
 /**
  * Created by nipunarora on 18/06/17.
  */
 
-public class FriendTab extends Fragment {
+public class FriendTab extends Fragment implements View.OnClickListener {
     View v;
     public FriendTab() {}
 
@@ -29,6 +32,18 @@ public class FriendTab extends Fragment {
         super.onActivityCreated(savedInstanceState);
         getFragmentManager().beginTransaction().replace(R.id.friendRequestsFrame,new FriendFrame()).commit();
         getFragmentManager().beginTransaction().replace(R.id.currentFriendsFrame,new FriendFrame()).commit();
+        AppCompatButton viewall_currentfriends=(AppCompatButton)v.findViewById(R.id.viewallCurrentfriends);
+        viewall_currentfriends.setOnClickListener(this);
 
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId())
+        {
+            case R.id.viewallCurrentfriends:Intent i =new Intent(getContext(), CurrentFriendList.class);
+                startActivity(i);
+             //TODO handle the viewall for friend requests as well
+        }
     }
 }
